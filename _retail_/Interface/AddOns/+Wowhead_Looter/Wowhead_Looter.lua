743b1119398845c5670fc082634fd689163c0e38
eTzmNcbkrng <eTzmNcbkrng@users.noreply.github.com>
2020-10-21 09:56:36 +1300

    Update

--- _retail_/Interface/AddOns/+Wowhead_Looter/Wowhead_Looter.lua
+++ _retail_/Interface/AddOns/+Wowhead_Looter/Wowhead_Looter.lua
@@ -3,15 +3,15 @@
 --     W o w h e a d   L o o t e r     --
 --                                     --
 --                                     --
---    Patch: 8.3.0                     --
---    Updated: August 18, 2020         --
+--    Patch: 9.0.1                     --
+--    Updated: October 14, 2020        --
 --    E-mail: feedback@wowhead.com     --
 --                                     --
 -----------------------------------------
 
 
 local WL_NAME = "|cffffff7fWowhead Looter|r";
-local WL_VERSION = 80300;
+local WL_VERSION = 90001;
 local WL_VERSION_PATCH = 0;
 local WL_ADDONNAME, WL_ADDONTABLE = ...
 
@@ -567,7 +567,7 @@ local GetArtifactInfoByRace = GetArtifactInfoByRace;
 local GetNumArchaeologyRaces = GetNumArchaeologyRaces;
 local GetNumArtifactsByRace = GetNumArtifactsByRace;
 local GetNumQuestLeaderBoards = GetNumQuestLeaderBoards;
-local GetNumQuestLogEntries = GetNumQuestLogEntries;
+local GetNumRaidMembers = GetNumRaidMembers;
 
 local GetPlayerMapPosition = function(unitToken)
     local uiMapID = C_Map.GetBestMapForUnit(unitToken) or WorldMapFrame:GetMapID();
@@ -591,7 +591,6 @@ local GetQuestLogLeaderBoard = GetQuestLogLeaderBoard;
 local GetQuestLogPushable = GetQuestLogPushable;
 local GetQuestLogSelection = GetQuestLogSelection;
 local GetQuestLogTimeLeft = GetQuestLogTimeLeft;
-local GetQuestLogTitle = GetQuestLogTitle;
 local GetRealZoneText = GetRealZoneText;
 local GetRewardText = GetRewardText;
 local GetSpellInfo = GetSpellInfo;
@@ -779,9 +778,9 @@ function wlEvent_PLAYER_LOGIN(self)
 
     -- load currencies
     for i=1, WL_CURRENCIES_MAXID do
-        local currencyName = GetCurrencyInfo(i);
-        if currencyName and currencyName ~= "" then
-            WL_CURRENCIES[currencyName:lower()] = i;
+        local cInfo = C_CurrencyInfo.GetCurrencyInfo(i);
+        if cInfo and cInfo.name and cInfo.name ~= "" then
+            WL_CURRENCIES[cInfo.name:lower()] = i;
         end
     end
     wlScanCurrencies();
@@ -1065,11 +1064,13 @@ end
 function wlEvent_GOSSIP_SHOW(self)
     wlEvent_BlockChatLoot(self);
     wlClearTracker("gossipNpc");
-    local gossips = { GetGossipOptions() };
+    local gossips = { C_GossipInfo.GetOptions() };
 
-    for i=1, #gossips, 2 do
-        if gossips[i + 1] ~= "gossip" then
-            wlRegisterUnitGossip(gossips[i + 1]);
+    for _,info in ipairs(gossips) do
+        for _,gossip in ipairs(info) do
+            if (gossip.type == "gossip") then
+                wlRegisterUnitGossip(gossip.name);
+            end
         end
     end
 end
@@ -1237,7 +1238,8 @@ function wlEvent_MERCHANT_UPDATE(self)
     local numCurrencies = #currencies;
     local currencyInfos = {};
     for index = 1, numCurrencies do
-        local cName, cCount, cIcon = GetCurrencyInfo(currencies[index]);
+        local cInfo = C_CurrencyInfo.GetCurrencyInfo(currencies[index]);
+        local cName, cCount, cIcon = cInfo.name, cInfo.quantity, cInfo.iconFileID;
         currencyInfos[cName] = { currencies[index], cIcon };
     end
 
@@ -1758,36 +1760,39 @@ function wlEvent_QUEST_LOG_UPDATE(self)
 
     local eventId = wlGetNextEventId();
     
-    local selectedQuest = GetQuestLogSelection();
-    
     wlCurrentQuestObj = 3 - wlCurrentQuestObj; -- toggle: 1 <-> 2
     
     for k, _ in ipairs(wlQuestObjectives[wlCurrentQuestObj]) do
         wlQuestObjectives[wlCurrentQuestObj][k] = nil;
     end
     
-    local foundQuests, numQuests, idx, objCounter = 0, select(2, GetNumQuestLogEntries()), 1, 0;
+    local foundQuests, numQuests, idx, objCounter = 0, select(2, C_QuestLog.GetNumQuestLogEntries()), 1, 0;
     while foundQuests <= numQuests do
         local title, isHeader, questId;
-        local questLogTitle = { GetQuestLogTitle(idx) };
-        if not questLogTitle[1] then
+        local questInfo = C_QuestLog.GetInfo(idx);
+        if not questInfo then
             break;
         end
-        title       = questLogTitle[1];
-        isHeader    = questLogTitle[4];
-        questId     = questLogTitle[8];
+        title       = questInfo.title
+        isHeader    = questInfo.isHeader;
+        questId     = questInfo.questID;
         if not isHeader then
-            SelectQuestLogEntry(idx);
         
             foundQuests = foundQuests + 1;
         
             wlUpdateVariable(wlQuestLog, foundQuests, "title", "set", title);
             wlQuestLog[foundQuests].id = questId;
-            wlQuestLog[foundQuests].timer = ceil((GetQuestLogTimeLeft() or 0) / 15) * 15;
-            wlQuestLog[foundQuests].sharable = GetQuestLogPushable() or 0;    
 
+            local totalTime, elapsedTime = C_QuestLog.GetTimeAllowed(questId);
+            totalTime = totalTime or 0;
+            elapsedTime = totalTime or 0;
+            wlQuestLog[foundQuests].timer = ceil(((totalTime - elapsedTime) or 0) / 15) * 15;
+            wlQuestLog[foundQuests].sharable = C_QuestLog.IsPushableQuest(questId) or 0;    
+
+            local objInfos = C_QuestLog.GetQuestObjectives(questId);
             for objId=1, GetNumQuestLeaderBoards(idx) do
-                local _, kind, done = GetQuestLogLeaderBoard(objId, idx);
+                local objInfo = objInfos[objId];
+                local kind, done = objInfo.type, objInfo.finished;
 
                 if kind == "event" then
                 
@@ -1808,8 +1813,6 @@ function wlEvent_QUEST_LOG_UPDATE(self)
         end
         idx = idx + 1;
     end
-    
-    SelectQuestLogEntry(selectedQuest);
     
     if wlTracker.quest.time and wlTracker.quest.action == "accept" then
         local i = wlTableFind(wlQuestLog, function(a, v) return a.id and a.id == v; end, wlTracker.quest.id);
@@ -1971,7 +1974,7 @@ end
 -- Event handler for QUEST_ITEM_UPDATE, which fires after the game client gathered quest requirement/reward item info.
  ]]
 function wlEvent_QUEST_ITEM_UPDATE(self)
-    local tagId = GetQuestTagInfo(GetQuestID())
+    local tagId = C_QuestLog.GetQuestTagInfo(GetQuestID())
 
     -- Check for warfront contribution tag
     if tagId == 153 then
@@ -2001,7 +2004,7 @@ function wlEvent_QUEST_PROGRESS(self)
     wlTracker.quest.time = wlGetTime();
     wlTracker.quest.action = "progress";
 
-    local tagId = GetQuestTagInfo(wlTracker.quest.id)
+    local tagId = C_QuestLog.GetQuestTagInfo(wlTracker.quest.id)
 
     -- Check for warfront contribution tag
     if tagId == 153 then
@@ -2127,9 +2130,8 @@ function wlRegisterQuestReturn()
     wlEvent[wlId][wlN][eventId].choiceItemLinks = wlTracker.quest.choiceItemLinks;
     wlEvent[wlId][wlN][eventId].rewardCurrencies = wlTracker.quest.rewardCurrencies;
     wlEvent[wlId][wlN][eventId].playerLevel = UnitLevel("player");
-    wlEvent[wlId][wlN][eventId].playerWarModeActive = UnitIsWarModeActive("player");
-    wlEvent[wlId][wlN][eventId].playerWarModeDesired = UnitIsWarModeDesired("player");
-    wlEvent[wlId][wlN][eventId].playerWarModePhased = UnitIsWarModePhased("player");
+    wlEvent[wlId][wlN][eventId].playerWarModeActive = C_PvP.IsWarModeActive();
+    wlEvent[wlId][wlN][eventId].playerWarModeDesired = C_PvP.IsWarModeDesired();
 
     wlTracker.quest.time = wlGetTime();
     wlTracker.quest.eventId = eventId;
@@ -2363,7 +2365,7 @@ function wlSeenWorldQuests()
 
     -- Now pick up emissary quests, since they're not returned in GetQuestsForPlayerByMapID
     for i = 1, #WL_WORLD_QUEST_EMISSARY_MAPS, 1 do
-        local rows = GetQuestBountyInfoForMapID(WL_WORLD_QUEST_EMISSARY_MAPS[i])
+        local rows = C_QuestLog.GetBountiesForMapID(WL_WORLD_QUEST_EMISSARY_MAPS[i])
         if rows then
             for rowIdx = 1, #rows, 1 do
                 addWorldQuestLine(rows[rowIdx].questID)
@@ -2381,22 +2383,53 @@ end
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
 function wlSeenIslandExpeditions()
-
-    -- BFA 8.2 GetTextureWithStateVisualizationInfo renamed to GetTextureAndTextVisualizationInfo
-    local textureFunc = C_UIWidgetManager.GetTextureWithStateVisualizationInfo or C_UIWidgetManager.GetTextureAndTextVisualizationInfo;
-    if (not textureFunc) then
-        return;
-    end
-
     -- found in Blizzard_IslandsQueueUI.lua
     local ISLANDS_QUEUE_WIDGET_SET_ID = 127;
 
+    -- Blizz changed from providing the ID to providing the name. Cute.
+    local lookup = {
+        ["islands-queue-card-dreadchain-icetroll"] = 5119,
+        ["islands-queue-card-skitteringhollow-troggs"] = 5121,
+        ["islands-queue-card-moltencay-ogre"] = 5122,
+        ["islands-queue-card-dreadchain-mogu"] = 5127,
+        ["islands-queue-card-dreadchain-kvaldir"] = 5128,
+        ["islands-queue-card-moltencay-jungletroll"] = 5129,
+        ["islands-queue-card-moltencay-sandtroll"] = 5130,
+        ["islands-queue-card-moltencay-yaungol"] = 5131,
+        ["islands-queue-card-rottingmire-jinyu"] = 5132,
+        ["islands-queue-card-rottingmire-saurok"] = 5133,
+        ["islands-queue-card-rottingmire-strandedpirates"] = 5134,
+        ["islands-queue-card-skitteringhollow-kobolds"] = 5135,
+        ["islands-queue-card-skitteringhollow-nerubians"] = 5136,
+        ["islands-queue-card-ungolruins-hozen"] = 5138,
+        ["islands-queue-card-ungolruins-pygmy"] = 5139,
+        ["islands-queue-card-ungolruins-quilboar"] = 5140,
+        ["islands-queue-card-verdantwilds-druids"] = 5142,
+        ["islands-queue-card-verdantwilds-furbolgs"] = 5143,
+        ["islands-queue-card-verdantwilds-keepers"] = 5144,
+        ["islands-queue-card-whisperingreef-makrura"] = 5146,
+        ["islands-queue-card-whisperingreef-murlocs"] = 5147,
+        ["islands-queue-card-whisperingreef-naga"] = 5148,
+        ["islands-queue-card-havenswood-cultist"] = 5207,
+        ["islands-queue-card-havenswood-faceless"] = 5209,
+        ["islands-queue-card-havenswood-worgen"] = 5210,
+        ["islands-queue-card-jorundall-taunka"] = 5212,
+        ["islands-queue-card-jorundall-vargul"] = 5213,
+        ["islands-queue-card-jorundall-vrykul"] = 5214,
+        ["islands-queue-card-snowblossom-mogu"] = 5261,
+        ["islands-queue-card-snowblossom-mantid"] = 5263,
+        ["islands-queue-card-snowblossom-vermin"] = 5264,
+        ["islands-queue-card-crestfall-pirates"] = 5265,
+        ["islands-queue-card-crestfall-orc"] = 5267,
+        ["islands-queue-card-crestfall-dragon"] = 5268,
+    }
+
     for i,widget in ipairs(C_UIWidgetManager.GetAllWidgetsBySetID(ISLANDS_QUEUE_WIDGET_SET_ID)) do
-        local info = textureFunc(widget.widgetID);
+        local info = C_UIWidgetManager.GetTextureAndTextVisualizationInfo(widget.widgetID)
         if info and info.shownState ~= 0 then
-            local textureId = info.portraitTextureKitID or info.textureKitID;
-            if (textureId) then
-                wlSeenDaily('t'..textureId);
+            local textureName = info.textureKit
+            if textureName and lookup[textureName] then
+                wlSeenDaily('t'..lookup[textureName])
             end
         end
     end
@@ -3420,7 +3453,8 @@ function wlEvent_CURRENCY_DISPLAY_UPDATE(...)
                 end
                 
                 -- make sure the player isn't capped
-                local currencyName, currentQ, currencyIcon, currencyEarnedThisWeek, currencyEarnablePerWeek, currencyCap, currencyIsDiscovered = GetCurrencyInfo(currencyId);
+                local cInfo = C_CurrencyInfo.GetCurrencyInfo(currencyId);
+                local currencyName, currentQ, currencyIcon, currencyEarnedThisWeek, currencyEarnablePerWeek, currencyCap, currencyIsDiscovered = cInfo.name, cInfo.quantity, cInfo.iconFileID, cInfo.quantityEarnedThisWeek, cInfo.maxWeeklyQuantity, cInfo.maxQuantity, cInfo.discovered;
                 if currentQ == currencyCap or (currencyEarnablePerWeek ~= 0 and currencyEarnedThisWeek == currencyEarnablePerWeek) then
                     return;
                 end
@@ -3510,7 +3544,8 @@ function wlScanCurrencies()
 
     -- collect current currency information
     for k, currencyID in pairs(WL_CURRENCIES) do
-        local localized_label, amount = GetCurrencyInfo(currencyID);
+        local cInfo = C_CurrencyInfo.GetCurrencyInfo(currencyID);
+        local localized_label, amount = cInfo.name, cInfo.quantity;
         if localized_label and "" ~= localized_label and k == localized_label:lower() and amount > 0 then
             scannedCurrencies[currencyID] = amount;
         end
@@ -4023,7 +4058,7 @@ function wlScanAppearances()
     -- enable filter if wardrobe frame is invisible.
     local enableFilter = not WardrobeCollectionFrame or not WardrobeCollectionFrame:IsVisible();
 
-    for colType = 1, NUM_LE_TRANSMOG_COLLECTION_TYPES do
+    for colType = 1, Enum.TransmogCollectionTypeMeta.NumValues do
         local app = wlGetCollectedTransmogAppearances(colType, enableFilter)
 
         for k, o in pairs(app) do
@@ -5400,7 +5435,7 @@ end
 --**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--**--
 
 function wlGetDate()
-    local date = C_Calendar.GetDate();
+    local date = C_DateAndTime.GetCurrentCalendarTime();
 
     return date.month, date.monthDay, date.year;
 end
